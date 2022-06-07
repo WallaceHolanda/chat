8e1c6e63bdea6f663e9eac2f893eb01664f0f4da
Wallace Holanda <wallace.holanda@hotmail.com>
2022-06-07 15:25:06 -0300

    Definindo a interface e os campos da tela de cadastro

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+// ignore: prefer_const_constructors
+
+import 'package:chat/components/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: SingleChildScrollView(
+          child: AuthForm(),
+        ),
+      ),
+    );
+  }
+}
